7c478e06f3bc2aa87e91144c6bdb66ceff63a555
Nicolajam <Nicolajam@users.noreply.github.com>
2023-12-26 07:44:18 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,71 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('Server running at http://localhost:3000/');
+});
+
+// Path: comments.js
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => { // 1
+    res.statusCode = 200; // 2
+    res.setHeader('Content-Type', 'text/plain'); // 3
+    res.end('Hello, world!'); // 4
+});
+
+server.listen(port, hostname, () => { // 5
+    console.log(`Server running at http://${hostname}:${port}/`); // 6
+});
+
+// 1. The createServer() method creates the HTTP server.
+// 2. The statusCode property sets the HTTP status code to 200, which means that all is OK.
+// 3. The setHeader() method sets an HTTP header with the content type.
+// 4. The end() method ends the response process.
+// 5. The server is listening on port 3000.
+// 6. The hostname and port the server is running on is logged to the console.
+
+// Path: comments.js
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => { // 1
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// 1. The createServer() method creates the HTTP server.
+// 2. The statusCode property sets the HTTP status code to 200, which means that all is OK.
+// 3. The setHeader() method sets an HTTP header with the content type.
+// 4. The end() method ends the response process.
+// 5. The server is listening on port 3000.
+// 6. The hostname and port the server is running on is logged to the console.
+
+// Path: comments.js
+const http = require('http'); // 1
+
+const hostname = 'localhost'; // 2
+const port = 3000; // 3
+
+const server = http.createServer((req, res) => { // 4
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, world!');
+});
+
+server.listen(port, hostname, () =>
